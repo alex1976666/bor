3c22fde738e1a87422df9d40ebef5d4573814acf
alex1976666 <alex1976666@mail.ru>
2015-07-23 19:10:18 +0400

    php

---
+++ feedback.php
@@ -0,0 +1,203 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+  <head>
+   
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="img/fav.png" type="image/x-icon">
+    <link rel="icon" href="img/fav.png" type="image/x-icon">
+
+  
+    <title>Гравировальные бормашины в Санкт-Петербурге</title>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="owl.carousel/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="owl.carousel/owl-carousel/owl.theme.css">
+    <link rel="stylesheet" href="owl.carousel/owl-carousel/owl.transitions.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="animate.css-master/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+            <div class="logo">
+              <a href="index.php"><strong>-AirBor-</strong></a>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+            <nav class="main-nav">
+              <ul>
+                <li class="main-nav__item">
+                  <a href="index.php">На главную</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+
+
+    <main>
+      <div class="hover"></div>
+
+      <div class="feedback-page">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-xs-12">
+  <?php
+
+$name=$_POST[name3];//переменная имени
+$otz=$_POST[otz];//переменная комментария              
+$l="<hr>";
+
+//очистка комментариев------------------------------------
+
+if ($otz=="delete") {$f=fopen("basa777.txt","w");
+ftruncate($f,0);
+fclose($f);}
+
+// дата время
+$today=getdate();
+$hours=$today['hours'];
+$minutes=$today['minutes'];
+$seconds=$today['seconds'];
+$vr=$today['mday']." :".$today['mon']." :".$today['year']."г. ".$today['hours'].":".$today['minutes'].":".$today['seconds'].".";
+
+$rez="<blockquote>".$otz."<br>".$vr."<cite>".$name."</cite></blockquote>"."\n";
+
+if ($name<>"") {$fl5=7;}
+if ($otz<>"") {$fl6=7;}
+
+$dop=$fl5.$fl6;
+if ($dop=="77") {
+//запись
+
+$filename="basa777.txt";
+
+$a="<br> ";
+
+
+if(is_writable($filename)) {
+
+if(!$handle=fopen($filename,"a")) {echo "ошибка"; exit;}
+
+if(fwrite($handle,$rez)===FALSE) {echo "ошибка2"; exit;}
+
+fclose ($handle);}
+
+else {echo "ошибка3";}
+}
+
+//чтение
+
+$file_array=file("basa777.txt");//Считать файл текста в массив
+
+$bs=$file_array;//запись файла в массив
+
+$bs=array_reverse($bs);
+
+//цикл
+
+for($nn=0;$nn<=count($bs);$nn=$nn+1) {echo $bs[$nn];}
+
+
+            
+?>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </main>
+
+
+    <!-- Footer -->
+
+
+    <footer>
+      <div class="contacts" id="contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <h2>Наши контакты</h2>
+              <ul>
+                <li>
+                  <img src="img/telephone5.svg" class="footer-phone" alt="phone">
+                  <a href="tel:+79118376607">+7(911)837-66-07</a>
+                </li>
+                <li>
+                  <img src="img/telephone5.svg" class="footer-phone" alt="phone">
+                  <a href="tel:+79643983800">+7(964)398-38-00</a>
+                </li>
+                <li>
+                  <a href="mailto:airbor.spb@gmail.com">airbor.spb@gmail.com</a>
+                </li>
+                <li>
+                  <a href="http://vk.com/airbor" target="_blank">Группа в ВК</a>
+                </li>
+                <li>Санкт-Петербург 2015</li>
+              </ul>
+              <div class="footer-logo">
+                -AirBor-
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Modals -->
+
+    <div class="recall">
+      <form action="#" method="post">
+      <p class="form-text">Спасибо,<br> мы Вам перезвоним</p>
+        <p><input type="text" name="name" placeholder="Ваше имя"></p>
+        <p><input type="tel" name="phone" placeholder="Ваш телефон" required></p>
+        <button type="submit" class="recall-btn" id="call-me-2">Позвоните мне</button>
+      </form>
+    </div>
+
+    <div class="order">
+      <form action="#" method="post" class="clearfix">
+        <div class="quantity">
+          <p class="clearfix"><label for="number">Количество шт.</label><input type="text" id="number" name="quantity" required></p>
+        </div>
+        <div class="client-data">
+        <p class="fill">Заполните поля,<br> чтобы мы с Вами связались</p>
+          <p><label for="name">Ваше имя</label><input type="text" id="name" placeholder="Имя" required></p>
+          <p><label for="phone">Ваш телефон</label><input type="text" id="phone" placeholder="Телефон" required></p>
+          <p><label for="city">Ваш город</label><input type="text" id="city" placeholder="Город" required></p>
+          <p><label for="mail">Ваш E-Mail</label><input type="text" id="mail" placeholder="E-Mail"></p>
+        </div>
+        <button type="submit" class="order-btn" id="order-2">Заказать</button>
+      </form>
+    </div>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,516 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+  <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="description" content="Продажа и доставка пневматических гравировальных мини-бормашин в Санкт-Петербурге и Ленинградской области">
+    <meta name="keywords" content="пневмоинструмент,инструмент,мини-бормашина,гравировка,бормашинка карандашного типа,бормашина,micro air grinder,revtool,mettler">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="img/fav.png" type="image/x-icon">
+    <link rel="icon" href="img/fav.png" type="image/x-icon">
+
+  
+    <title>Купить пневматические гравировальные бормашины в Санкт-Петербурге</title>
+
+  
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="owl.carousel/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="owl.carousel/owl-carousel/owl.theme.css">
+    <link rel="stylesheet" href="owl.carousel/owl-carousel/owl.transitions.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="animate.css-master/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+            <div class="logo">
+              <strong>-AirBor-</strong>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+            <nav class="main-nav">
+              <ul>
+                <li class="main-nav__item"><a href="#complect">Описание и Комплектация</a></li>
+                <li class="main-nav__item"><a href="#video">Видео</a></li>
+                <li class="main-nav__item"><a href="#delivery">Доставка</a></li>
+                <li class="main-nav__item"><a href="#feedback">Отзывы</a></li>
+                <li class="main-nav__item"><a href="#contact">Контакты</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+
+
+    <main>
+    <div class="hover"></div>
+
+    <!-- Promo -->
+
+      <section class="promo">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-xs-12 col-md-5 col-lg-5">
+              <div class="cover">
+                <h1 class="promo-title">Гравировка это интересно!</h1>
+                  <div class="promo-text">
+                    <p>
+                      С появлением такого инструмента, как гравировальная мини-бормашина или попросту гравировальный карандаш, гравировать стало еще легче и приятней. Главным достоинством считается высокая мощность пневмодвигателя при небольшом размере и массе.
+                    </p>
+                    <p>
+                      Высокие обороты позволяют работать плавно и без вибраций, а благодаря размерам ее очень удобно держать в руке, что сказывается на качестве выполняемой работы.
+                    </p>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-xs-12 col-md-7 col-lg-7">
+
+              <div class="img-box">
+                <div class="images images-1">
+                  <img src="img/2.png" alt="Гравировальная бормашина">
+                </div>
+                 <span>+</span>
+                <div class="images images-2">
+                  <img src="img/bor.png" alt="Набор боров">
+                </div>
+                <div class="old-price">
+                <p>Весь Июль !</p>
+                6490 Р
+                  <div class="del"></div>
+                </div>
+                <div class="price-box">
+                  <img src="img/price1.svg" class="badge" alt="Бейджик">
+                  <p>
+                    5500 Р
+                  </p><br>
+                  
+                  <button class="order-btn animated shake" id="order"><img src="img/buy10.svg" alt="">Заказать</button>
+                </div>
+
+                
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- Call Me -->
+
+
+      <div class="callback">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-md-8 col-lg-8">
+              <img src="img/operator4.svg" class="operator" alt="callback">
+              <span>Если у Вас возникли вопросы или просто лень заполнять форму отправки, Вы можете заказать обратный звонок.</span>
+            </div>
+            <div class="col-xs-12 col-md-4 col-lg-4">
+              <button class="recall-btn" id="call-me"><img src="img/cellphone96.svg" alt="recall">Перезвоните мне</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="advantages">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-md-4 col-lg-4">
+            <p><img src="img/free9.svg" alt="Бесплатная доставка"></p>
+              Бесплатная доставка по СПб<br> в день заказа
+            </div>
+
+            <div class="col-xs-12 col-md-4 col-lg-4">
+            <p><img src="img/guarantee.svg" alt="Гарантия"></p>
+              Гарантия на товар 6 мес.<br> с момента покупки
+            </div>
+
+            <div class="col-xs-12 col-md-4 col-lg-4">
+            <p><img src="img/heart316.svg" alt="Почтение"></p>
+              Мы ценим своих покупателей!
+            </div>
+          </div>
+        </div>
+      </div>
+
+     <section class="about">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-md-6 col-lg-6">
+              <div class="use">
+                <h2>Удобно работать</h2>
+                <p>
+                  Машинка работает на сжатом воздухе, что обеспечивает большой рабочий ресурс и способность работать долго без остановки. Корпус инструмента выполнен в удобной форме, размером с маркер. Благодаря этому работать можно продолжительное время без всякого дискомфорта. Иными словами пневмоинструмент обеспечивает высокую производительность труда, при низкой утомляемости персонала.
+Однако, для длительной и продуктивной эксплуатации пневмоинструмента, необходимо соблюдать технические требования, в которых указан режим смазки и рекомендованные смазочные материалы.
+                </p>
+              </div>
+              </div>
+              <div class="col-xs-12 col-md-6 col-lg-6">
+                <div class="product-advantages">
+                  <h2>Преимущества</h2>
+                  <ul>
+                    <li>Большой рабочий ресурс</li>
+                    <li>Надежность</li>
+                    <li>Безопасность использования</li>
+                    <li>Идеальное соотношение цены и продолжительности работы</li>
+                    <li>Не боится перегрузок, благодаря пневмодвигателю</li>
+                  </ul>
+                </div>
+              </div>
+            
+          </div>
+        </div>
+      </section>
+
+
+      <!-- Slider -->
+
+
+      <section class="slider" id="complect">
+        <div class="container">
+          
+          <div class="row">
+          <div class="col-xs-12 col-md-6 col-lg-6">
+          <h2>Описание</h2>
+            <div class="complectation">
+              <ul>
+                <li>Диаметр зажимной цанги - 3мм,</li>
+                <li>Рабочее давление - 6.3 бар,</li>
+                <li>Частота вращения - 56000 об/мин,</li>
+                <li>Длина - 132мм,</li>
+                <li>Рекомендуемый диаметр шланга - 6мм,</li>
+                <li>Тип инструмента - прямая бормашина,</li>
+                <li>Тип бормашины - карандашного типа,</li>
+                <li>Вес - 330гр.</li>
+              </ul>
+            </div>
+          </div>
+            <div class="col-xs-12 col-md-6 col-lg-6 psr">
+            <h2>Комплектация</h2>
+              <div id="owl-demo" class="owl-carousel my-set">
+                <div><img src="img/slider-1.png" alt="image"></div>
+                <div><img src="img/slider-7.png" alt="image"></div>
+                <div><img src="img/slider-2.png" alt="image"></div>
+                <div><img src="img/slider-3.png" alt="image"></div>
+                <div><img src="img/slider-5.png" alt="image"></div>
+                <div><img src="img/slider-4.png" alt="image"></div>
+                <div><img src="img/slider-6.png" alt="image" class="mt"></div>
+                <div><img src="img/mbox.jpg" alt="image"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- Video -->
+
+
+      <section class="video" id="video">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-md-6 col-lg-6">
+            <h2>Видео</h2>
+              <div class="video-box">
+                <iframe class="youtube" src="https://www.youtube.com/embed/PKNAWr-BlOk" allowfullscreen></iframe>
+              </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-6">
+              <div class="video-description">
+                <p>
+                  В данном видео показана работа мини-бормашинки твердосплавным бором по натуральному камню.
+                  Может применяться также и к другим материалам.
+                  Скорость вращения 56000 об/мин.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div class="delivery-pay" id="delivery">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-md-6 col-lg-6">
+              <section class="delivery">
+              <h2>Доставка</h2>
+                <ul>
+                  <li>
+                    <img src="img/delivery-truck5.svg" alt="Грузовик">
+                  </li>
+                  <li>
+                    Доставка осуществляется с 10.00 до 20.00.
+                  </li>
+                  <li>
+                    Доставка в регионы при условии 100% предоплаты.
+                  </li>
+                </ul>
+              </section>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-6">
+              <section class="pay">
+                <h2>Оплата</h2>
+                <ul>
+                  <li><img src="img/pay.svg" alt="Оплата"></li>
+                  <li>Оплата заказа при доставке по СПб осуществляется наличными курьеру при получении.</li>
+                  <li></li>
+                </ul>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Feedback -->
+
+
+      <div class="feedback" id="feedback">
+        <div class="container">
+          <div class="row">
+          <div class="col-xs-12">
+            <h2>Отзывы</h2>
+          </div>
+          
+            <div class="col-xs-12 col-md-6 col-lg-6">
+              <blockquote>
+                Очень удобная бормашина, советую всем попробовать!<cite>Александр</cite>
+              </blockquote>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-lg-6">
+              <blockquote>
+                Спасибо Вам! Порадовала цена и качество.Теперь буду брать оптом.<cite>Игорь</cite>
+              </blockquote>
+            </div>
+            <div class="col-xs-12">
+              
+              <div class="more-feedback">
+                <a href="feedback.php">Читать все отзывы >></a>
+              </div>
+            </div>
+
+           
+              <div class="col-xs-12">
+               <div class="make-feedback">
+              <h2>Напишите свой отзыв</h2>
+                <form name="form3" method="post" action="feedback.php">
+                  <p><input type="text" name="name3" id="feedback-name" placeholder="Ваше имя" required></p>
+                  <p><textarea name="otz" id="feedback-text" placeholder="Ваш отзыв" required></textarea></p>
+                  <p><input type="submit" id="feedback-btn"></p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+
+    <!-- Footer -->
+
+
+    <footer>
+      <div class="contacts" id="contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <h2>Наши контакты</h2>
+              <ul>
+                <li>
+                  <img src="img/telephone5.svg" class="footer-phone" alt="phone">
+                  <a href="tel:+79118376607">+7(911)837-66-07</a>
+                </li>
+                <li>
+                  <img src="img/telephone5.svg" class="footer-phone" alt="phone">
+                  <a href="tel:+79643983800">+7(964)398-38-00</a>
+                </li>
+                <li>
+                  <a href="mailto:airbor.spb@gmail.com ">airbor.spb@gmail.com</a>
+                </li>
+                <li>
+                  <a href="http://vk.com/airbor" target="_blank">Группа в ВК</a>
+                </li>
+                <li>Санкт-Петербург 2015</li>
+              </ul>
+              <div class="footer-logo">
+                -AirBor-
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Modals -->
+
+    <div class="recall">
+      <form name="form1" method="post" action="index.php">
+      <p class="form-text">Заполните форму</p>
+       <p><input type="text" name="name2" id="recall-name" placeholder="Ваше имя"></p>
+        <p><input type="tel" name="phone2" id="recall-phone" placeholder="Ваш телефон" required></p>
+        <button type="submit" class="recall-btn" id="call-me-2">Позвоните мне</button>
+      </form>
+      
+    </div>
+	<?php
+
+//$n2=$_POST[name2];
+//$p2=$_POST[phone2];
+
+//установка флагов------------------------------------
+
+if ($_POST[name2]<>"") {$f1=1;}//если имя не пустышка то флаг1 =1----------------
+if ($_POST[phone2]<>"") {$f2=1;}//если коммент не пустышка то флаг2 =1----------------
+
+$cod=$f1.$f2;
+
+//если scod=11 то только при этом условии производится запись--------------------
+
+if ($cod=="11") {
+	//отправка на почту введённых данных
+$zz='имя: '.$_POST['name2']."\r\n";
+$zz=$zz.'тел: '.$_POST['phone2']."\r\n";
+mail ( 'alex1976spb@gmail.com', 'перезвоните мне' , $zz, 'From:AirBor' ); 
+}
+?>
+
+    <div class="recall-thanks">
+        Спасибо,<br> мы Вам перезвоним.
+      </div>
+
+    <div class="order">
+      <form name="form2" method="post" action="index.php" class="clearfix">
+        <div class="quantity">
+          <p class="clearfix"><label for="number">Количество шт.</label><input type="text" name="number" id="number" name="quantity" required></p>
+        </div>
+        <div class="client-data">
+        <p class="fill">Заполните поля,<br> чтобы мы с Вами связались</p>
+          <p><label for="name">Ваше имя</label><input type="text" name="name" id="order-name" placeholder="Имя" required></p>
+          <p><label for="phone">Ваш телефон</label><input type="text" name="phone" id="order-phone" placeholder="Телефон" required></p>
+          <p><label for="city">Ваш город</label><input type="text" name="city" id="city" placeholder="Город" required></p>
+          <p><label for="mail">Ваш E-Mail</label><input type="text" name="mail" id="mail" placeholder="E-Mail"></p>
+        </div>
+        <button type="submit" class="order-btn" id="order-2">Заказать</button>
+      </form>
+    </div>
+	<?php
+//$num=$_POST[number];
+//$n=$_POST[name];
+//$ph=$_POST[phone];
+//$go=$_POST[city];
+//$e=$_POST[mail];
+//$br="<br>";
+
+//$ob="кол-во".$num.$br.$n.$br.$ph.$br.$go.$br.$e.$br;
+
+
+if ($_POST[name]<>"") {$f5=3;}
+if ($_POST[phone]<>"") {$f6=3;}
+if ($_POST[city]<>"") {$f7=3;}
+if ($_POST[mail]<>"") {$f8=3;}
+
+$cod=$f5.$f6.$f7.$f8;
+
+//если $cod=3333 то только при этом условии производится запись--------------------
+
+if ($cod=="3333") {
+
+$mmm='кол-во: '.$_POST['number']."\r\n";
+$mmm=$mmm.'имя: '.$_POST['name']."\r\n";
+$mmm=$mmm.'телефон: '. $_POST['phone']."\r\n";
+$mmm=$mmm.'город: '.$_POST['city']."\r\n";
+$mmm=$mmm.'e-mail: '.$_POST['mail']."\r\n";
+mail ( 'alex1976spb@gmail.com', 'заказ' , $mmm, 'From:AirBor' );
+}
+?>
+
+    <div class="order-set">
+    <div class="order-set__img">
+      <img src="img/smiling36.svg" alt="Смайлик">
+    </div>
+      Спасибо,<br> Ваш заказ принят.
+    </div>
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="owl.carousel/owl-carousel/owl.carousel.js"></script>
+
+    <script>
+      $(document).ready(function() {
+       
+        $("#owl-demo").owlCarousel({
+          autoPlay : true,
+          stopOnHover : true,
+          navigation:true,
+          paginationSpeed : 1000,
+          goToFirstSpeed : 2000,
+          singleItem : true,
+          autoHeight : false,
+          transitionStyle:"fade"
+        });  
+      });
+
+      $("#call-me").click(function(e){
+        e.preventDefault();
+        $(".recall, .hover").fadeIn();
+        });
+
+      $("#call-me-2").click(function(e){
+        e.preventDefault();
+        $(".recall").fadeOut();
+        $(".recall-thanks").fadeIn();
+        });
+     
+
+      $("#order").click(function(e){
+        e.preventDefault();
+        $(".order, .hover").fadeIn();
+      }); 
+
+      $("#order-2").click(function(e){
+        e.preventDefault();
+        $(".order").fadeOut();
+        $(".order-set").fadeIn();
+      });
+
+
+      $(".hover, .order-2").click(function(){
+        $(".recall, .order, .recall-thanks, .order-set, .hover").fadeOut();
+      });
+    </script>
+
+  </body>
+</html>
